e85311c8a741a3c7d15918d6cf986a526112f351
ditokapanadze <ditokapanadze111@gmail.com>
2021-06-02 00:19:48 -0700

    reg popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,6 @@
 var auth_modal = document.getElementById("auth-modal");
 var reg_modal = document.getElementById("reg-modal");
 
-
 // Get the button that opens the modal
 var auth_btn = document.getElementById("auth-btn");
 var reg_btn = document.getElementById("reg-btn");
@@ -11,30 +10,28 @@ var reg_btn = document.getElementById("reg-btn");
 var span_close_1 = document.getElementsByClassName("close")[0];
 var span_close_2 = document.getElementsByClassName("close")[1];
 
-
 // When the user clicks on the button, open the modal
-auth_btn.onclick = function() {
-    auth_modal.classList.add("d-block");
-}
+auth_btn.onclick = function () {
+  auth_modal.classList.add("d-block");
+};
 
-reg_btn.onclick = function() {
-    reg_modal.classList.add("d-block");
-}
+reg_btn.onclick = function () {
+  reg_modal.classList.add("d-block");
+};
 
 // When the user clicks on <span> (x), close the modal
-span_close_1.onclick = function() {
-    auth_modal.classList.remove("d-block");
-}
-span_close_2.onclick = function() {
-    red_modal.classList.remove("d-block")
-}
+span_close_1.onclick = function () {
+  auth_modal.classList.remove("d-block");
+};
+span_close_2.onclick = function () {
+  reg_modal.classList.remove("d-block");
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == auth_modal) {
-        auth_modal.classList.remove("d-block");
-    }
-    else if (event.target == reg_modal){
-        reg_modal.classList.remove("d-block")
-    }
-}
+window.onclick = function (event) {
+  if (event.target == auth_modal) {
+    auth_modal.classList.remove("d-block");
+  } else if (event.target == reg_modal) {
+    reg_modal.classList.remove("d-block");
+  }
+};
