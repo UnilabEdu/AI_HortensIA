0a9f7ffa4856dcc98f51b6eb5e7301ee0dc8795b
Denis Santuryan <79437472+Denissant@users.noreply.github.com>
2021-06-03 21:27:24 +0400

    გავასწორე ბაზის მოდელები და დავამატე Populate command-ი (#11)
    * fixed: from pycparser==2 to pycparser==2.20 in requirements.txt
    
    * fixed models, requirements and added populate-with-random-data command

--- project/commands/__init__.py
+++ project/commands/__init__.py
@@ -2,9 +2,10 @@ from project import create_app
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 from .db_init import InitDbCommand
-
+from .populate_random import PopulateWithRandomCommand
 
 manager = Manager(create_app())
 
 manager.add_command('db', MigrateCommand)
 manager.add_command('db_init', InitDbCommand)
+manager.add_command('populate_with_random', PopulateWithRandomCommand)
---
+++ project/commands/populate_random.py
@@ -0,0 +1,101 @@
+from flask_script import Command
+from project.models import db, Emotion, Ticket, Text
+from project.models.user import UserModel
+from essential_generators import DocumentGenerator
+from random import randrange, choice
+from flask import current_app
+from datetime import datetime, timedelta
+
+
+gen = DocumentGenerator()  # used to generate random words and sentences
+
+
+class PopulateWithRandomCommand(Command):
+    """
+    Populates DB with Users and Texts with randomized data, Emotions,
+    and Tickets related to those Users, Texts and Emotions
+    """
+
+    def run(self):
+        # db.create_all()  # uncomment if db_init wasn't used and DB wasn't populated yet
+        populate_with_random()
+
+
+def populate_with_random():
+    populate_users()
+    populate_texts()
+    populate_emotions()
+    populate_tickets()
+
+    db.session.commit()
+
+
+def populate_users():
+    #  Generate a hashed password
+    user_manager = current_app.user_manager
+    hashed_password = user_manager.hash_password('password')
+
+    users_amount = 30  # amount of users to generate
+
+    usernames = ['User_' + gen.word().capitalize() + str(randrange(1, 2003)) for i in range(users_amount)]
+    emails = [gen.word()[:3] + gen.email() for i in range(users_amount)]
+
+    confirmed_at = datetime.now()
+    active = 1
+
+    for username, email in zip(usernames, emails):
+        db.session.add(UserModel(username=username,
+                                 password=hashed_password,
+                                 email=email,
+                                 confirmed_at=confirmed_at,
+                                 active=active))
+
+
+def populate_texts():
+    for i in range(50):
+        db.session.add(Text(text=gen.sentence()))
+
+
+def populate_emotions():
+    emotions_list = [  # Primary Emotions
+        "Rage", "Anger", "Annoyance",
+        "Vigilance", "Anticipation", "Interest",
+        "Ecstasy", "Joy", "Serenity",
+        "Admiration", "Trust", "Acceptance",
+        "Terror", "Fear", "Apprehension",
+        "Amazement", "Surprise", "Distraction",
+        "Grief", "Sadness", "Pensiveness",
+        "Loathing", "Disgust", "Boredom",
+        # Secondary Emotions
+        'Aggressiveness', 'Optimism', 'Love', 'Submission', 'Awe', 'Disapproval', 'Remorse', 'Contempt'
+    ]
+    examples = ['I feel ' + emotion for emotion in emotions_list]
+
+    for emotion, example in zip(emotions_list, examples):
+        db.session.add(Emotion(name=emotion,
+                               synonym=emotion,
+                               example=example))
+
+
+def populate_tickets():
+    emotion_id_list = range(1, 33)  # IDs of all emotions
+
+    date_end = datetime.now()  # max date
+    date_max_difference = 30  # max difference (in days) between today and Ticket submission date
+
+    # Iterate over 30 User IDs
+    for user_id in range(1, 31):
+        text_id_list = list(range(1, 51))
+        filled_tickets = randrange(15, 35)  # amount of tickets for current user
+
+        for i in range(filled_tickets):
+            text_id = choice(text_id_list)  # choose a random text to assign to current ticket
+            text_id_list.remove(text_id)  # remove the used ticket from the next iteration for current user
+            emotion_id = choice(emotion_id_list)  # choose a random emotion to assign to current ticket
+            date = date_end - timedelta(days=randrange(0, date_max_difference))
+
+            ticket = Ticket(user_id=user_id,
+                            text_id=text_id,
+                            emotion_id=emotion_id,
+                            date=date)
+            db.session.add(ticket)
