00d59424f83a93baff2460a79a9c3a62b7ac25bf
tskaro <tskaro@gmail.com>
2021-06-13 23:48:17 +0400

    Front_integration 1.0 (#16)
    * created front_integration directory
    
    * created base.html
    
    * adding landing_page
    
    * merged with sandros work
    
    * merged with sandros work
    
    * started working on landing page
    
    * took front files from front branch
    
    * created static repository. integrated jinja urls for static files in home html. created homepage_blueprint
    
    * added babel to the project and did first translations
    
    * added languages, subscribe_form generated by flask
    
    * made correction in home html

---
+++ project/front_integration/views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, request, session
+from project import babel, Config
+from subscribe_function.subscribe_form import subscribe_form
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(Config.LANGUAGES.keys())
+    # return "en"
+
+
+homepage_blueprint = Blueprint('homepage',
+                               __name__,
+                               template_folder='templates'
+                               )
+
+
+@homepage_blueprint.route('/', methods=['GET', 'POST'])
+def home():
+    email = None
+    form = subscribe_form()
+
+    if form.validate_on_submit():
+        session['email'] = email
+        print(session['email'])
+
+    return render_template('home.html', form=form, email=email)
