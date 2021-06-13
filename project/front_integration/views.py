57cd1cbbbe4663e2ce12f79ab4b1a123560ea910
tskaro <tskaro@gmail.com>
2021-06-13 19:25:21 +0400

    added languages, subscribe_form generated by flask

--- project/front_integration/views.py
+++ project/front_integration/views.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, render_template
-from project import babel
+from flask import Blueprint, render_template, request, session
+from project import babel, Config
+from subscribe_function.subscribe_form import subscribe_form
+
 
 @babel.localeselector
 def get_locale():
-    return 'en'
+    return request.accept_languages.best_match(Config.LANGUAGES.keys())
+    # return "en"
+
 
 homepage_blueprint = Blueprint('homepage',
                                __name__,
@@ -13,4 +17,11 @@ homepage_blueprint = Blueprint('homepage',
 
 @homepage_blueprint.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    email = None
+    form = subscribe_form()
+
+    if form.validate_on_submit():
+        session['email'] = email
+        print(session['email'])
+
+    return render_template('home.html', form=form, email=email)
