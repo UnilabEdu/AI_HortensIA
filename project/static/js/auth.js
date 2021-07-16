e67fd93ee255eb9e9b6ca963b1f0fb64d67b3b75
Denissant <denissanturyan@gmail.com>
2021-07-16 06:51:33 +0400

    added login validator translations

--- project/static/js/auth.js
+++ project/static/js/auth.js
@@ -10,8 +10,6 @@ if (login_attempt) {
     reg_modal.classList.add("d-block");
 }
 
-// END
-
 
 // Email subscription functionality
 
@@ -46,3 +44,47 @@ email_submit_btn.addEventListener('click', function() {
         });
     }
 })
+
+
+// Translate validator texts
+
+let loginHelpTexts_en = ['Username is required', 'Email is required', 'Invalid Email',
+    'Password is required', 'Username/Email does not exist', 'Incorrect Password',
+    'Email address is required', 'Invalid Email address',
+    'This Email is already in use. Please try another one.', 'This Username is already in use. Please try another one.',
+    "Username may only contain letters, numbers, '-', '.' and '_'",
+    'Username must be at least 3 characters long',
+    'Password must have at least 6 characters with one lowercase letter, one uppercase letter and one number',
+    ]
+
+let loginHelpTexts_ka = ['შეიყვანეთ მომხმარებლის სახელი', 'შეიყვანეთ ელ-ფოსტა', 'ელ-ფოსტა არასწორადაა შეყვანილი',
+    'შეიყვანეთ პაროლი', 'მონაცემები არასწორადაა შეყვანილი', 'პაროლი არასწორია',
+    'შეიყვანეთ ელ-ფოსტა', 'ელ-ფოსტა არასწორადაა შეყვანილი',
+    'ელ-ფოსტის მისამართი დაკავებულია', 'მომხმარებლის სახელი დაკავებულია',
+    "გამოიყენეთ ასოები, ციფრები ან: -, . , _",
+    'გამოიყენეთ მინიმუმ 3 სიმბოლო',
+    'გამოიყენეთ 6+ სიმბოლო: დიდი/პატარა ასოები, ციფრები',
+    ]
+
+let loginHelpTexts_enShort = loginHelpTexts_en
+loginHelpTexts_enShort[8] = 'This Email is already in use';
+loginHelpTexts_enShort[9] = 'This Username is already in use';
+loginHelpTexts_enShort[10] = 'Use only letters, numbers, -, ., and _'
+loginHelpTexts_enShort[11] = 'Use at least 3 characters'
+loginHelpTexts_enShort[12] = 'Use 6+ characters: uppercase/lowercase letters, numbers'
+
+let helpTexts = document.querySelectorAll('.help-block')
+
+if (helpTexts) {
+    console.log('VALIDATORS DETECTED')
+    for (let i = 0; i < helpTexts.length; i++) {
+        let currentTextIndex = loginHelpTexts_en.indexOf(helpTexts[i])
+        console.log('VERY IMPORTANT FOR THE NEXT LINE TO BE DEFINED')
+        console.log(currentTextIndex)
+        if (current_lang === 'ka') {
+            helpTexts[i].innerHTML = loginHelpTexts_ka[currentTextIndex]
+        } else if (current_lang === 'en') {
+            helpTexts[i].innerHTML = loginHelpTexts_enShort[currentTextIndex]
+        }
+    }
+}
