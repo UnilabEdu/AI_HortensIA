5c828c1f4881e15cded16253ef16acf6ae4160f2
Denis Santuryan <79437472+Denissant@users.noreply.github.com>
2021-07-19 19:09:41 +0400

    Finished tickets, improved dashboard, login validators, endpoints (#28)
    * fixed: from pycparser==2 to pycparser==2.20 in requirements.txt
    
    * fixed models, requirements and added populate-with-random-data command
    
    * added frequencies line charts and unfinished heatmap
    
    * completed heatmap, working leaderboard (needs cleanup)
    
    * heatmap coloring change
    
    * radar in progress. bugged DB
    
    * new radar charts before removing old radar chart
    
    * radar data processing fixes
    
    * added all radar charts
    
    * async loading in progress
    
    * async loading testing
    
    * async loading done
    
    * put charts inside nav-tabs
    
    * preloading empty charts. started full migration to charts.js
    
    * fully migrated to chartjs. changed design
    
    * remaining radar charts preload now
    
    * leaderboard optimization, gradients
    
    * merged HTML files, small fixes
    
    * added streaks visualization
    
    * managed streaks exceptions: 0-day streak; yesterday's streak
    
    * fixed exception: activity charts when 0 filled tickets; modified populate
    
    * new chart: next leaderboard rank-up goal
    
    * added Files and Streaks models, modified Text model, updated Populate command
    
    * minor fixes to models and populate command
    
    * updated emotions model to bilingual. updated populate command accordingly
    
    * tiny fix
    
    * minor fix
    
    * partially implemented leaderboard data processing optimizations
    
    * weekly goal, weekly tables, streaks functions, fixes
    
    * added streaks chart, streaks updating and populating functions
    
    * quick fix: streaks chart
    
    * transferred frontend files, integrated static files
    
    * fixed requirements.txt
    
    * implemented user login functionality into frontend
    
    * renamed UserModel to User
    
    * implemented email subscription, added one-method api, organized apis
    
    * implemented flask-mail, fixed registration
    
    * translated most of index.html
    
    * added styles to dashboard weekly table
    
    * fixed babel
    
    * flask-babel toggle and dashboard frontend
    
    * finished dashboard frontend
    
    * dashboard: better buttons, tables, other fixes
    
    * structure, all redirects and endpoints, navbar
    
    * added login validator translations
    
    * implemented login validator translations. fixed register validators not appearing
    
    * minor frontend fixes
    
    * implemented emotion selectors
    
    * improved emotion wheel selectors and styles
    
    * ticket view finished: API, sentence display/update, animations
    
    Co-authored-by: Temur Chichua <69351709+temurchichua@users.noreply.github.com>

--- project/static/js/home.js
+++ project/static/js/home.js
@@ -3,14 +3,13 @@
 const profile = document.querySelector(".profile");
 const nav = document.querySelector(".nav");
 
-profile.addEventListener('click', () => {
-    console.log('asdfknsapdjf')
-  if (nav.classList.contains("d-none")){
-    nav.classList.remove("d-none");
-  } else {
-    nav.classList.add("d-none");
-  }
-});
+// profile.addEventListener('click', () => {
+//   if (nav.classList.contains("d-none")){
+//     nav.classList.remove("d-none");
+//   } else {
+//     nav.classList.add("d-none");
+//   }
+// });
 
 const leng_checkbox = document.getElementById('lang-checkbox');
 const geo = document.getElementById('geo');
@@ -18,12 +17,172 @@ const eng = document.getElementById('eng')
 
 leng_checkbox.addEventListener('change', () => {
   if (document.querySelector('#lang-checkbox:checked')) {
-    console.log('1');
     eng.classList.remove("lang__action");
     geo.classList.add("lang__action");
   } else {
-    console.log("2");
     geo.classList.remove("lang__action");
     eng.classList.add("lang__action");
   }
 });
+
+
+// Initialize the first sentence, create a function for displaying the next sentence
+
+const targetCardText = document.getElementById('target-card-text');
+const nextButton = document.getElementById('next-submit-ticket')
+
+var fetchNextText = async function () {
+    let response = await fetch('/api/ticketrequest');
+    return await response.json();
+};
+
+async function displayNextText() {
+    currentTextData = await fetchNextText();
+    console.log('displayNextText running')
+    console.log(currentTextData)
+    if (currentTextData.text) {
+        targetCardText.style.opacity = 1
+        targetCardText.innerHTML = currentTextData.text
+
+        targetCardText.animate([
+          { transform: 'translate3D(-2000px, 0, 0)' },
+          { transform: 'translate3D(0, 0, 0)' }
+        ], {
+          duration: 1500,
+          iterations: 1
+        })
+        targetCardText.style.right = 0
+        console.log(targetCardText.style.position)
+        targetCardText.style.position = 'static'
+
+        targetCardText.style.color = '#000000'
+        nextButton.style.opacity = 1
+    } else {
+        targetCardText.innerHTML = 'თქვენ ამოწურეთ მარკირებისთვის განკუთვნილი წინადადებების საცავი. ახალი წინადადებები მოგვიანებით დაემატება. '
+        targetCardText.style.color = '#320606'
+        nextButton.style.opacity = 0.5
+        nextButton.disabled = true
+    }
+}
+
+
+
+displayNextText()
+
+
+// Update Current Emotion Info by clicking on Selector Areas
+
+const emotionSelectors = document.querySelectorAll('.emotions');
+const title = document.getElementById('title');
+const synonym = document.getElementById('synonym');
+const about = document.getElementById('about');
+const help = document.getElementById('help');
+const aboutQuestion = document.getElementById('about-question');
+const helpQuestion = document.getElementById('help-question');
+
+const emotionAreaIdentifiers = ['38', '30', '29',
+  '28', '31', 'Group_1540',
+  '34', '17', '7',
+  '35', '11', '8',
+  '36', '12', '14',
+  '25', '21', '13',
+  '37', '15', '9',
+  'Group_1525', '19', '10',
+  '27', '26', '6', '5', '4', '3', '2', '1', '40']  // selectors for clickable areas corresponding to an emotion have these IDs
+const emptyAreaIdentifiers = ['33', '32', '16', '24', '23', '22', '20', '18']  // selectors for empty areas, not corresponding to any emotion
+
+
+fetch('/api/emotionlist')
+  .then((response) => {
+    if (!response.ok) {
+      throw Error('ERROR');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    emotionNamesData = data
+  })
+
+
+for (let i = 0; i < emotionSelectors.length; i++) {
+  emotionSelectors[i].addEventListener('click', () => {
+    if (!emptyAreaIdentifiers.includes(emotionSelectors[i].id)) {
+        let correctIndex = emotionAreaIdentifiers.indexOf(emotionSelectors[i].id)
+
+        title.innerHTML = emotionNamesData[correctIndex].emotion;
+        synonym.innerHTML = emotionNamesData[correctIndex].synonym;
+        about.innerHTML = emotionNamesData[correctIndex].example;
+        help.innerHTML = emotionNamesData[correctIndex].example;
+        aboutQuestion.innerHTML = "რას გვეუბნება " + emotionNamesData[correctIndex].emotion + ":"
+        helpQuestion.innerHTML = "როგორ დაგეხმარება " + emotionNamesData[correctIndex].emotion + ":"
+        nextButton.disabled = false
+
+        chosenEmotion = correctIndex
+  }
+  });
+}
+
+
+// Initialize function to reset the selected emotion after the emotion is submitted
+
+function resetSelectedEmotion() {
+    title.innerHTML = 'აირჩიეთ ემოცია'
+    synonym.innerHTML = 'არჩეული ემოციის სინონიმი'
+    about.innerHTML = 'არჩეული ემოციის განმარტება'
+    help.innerHTML = 'არჩეული ემოციის მნიშვნელობის განმარტება'
+    aboutQuestion.innerHTML = 'რას გვეუბნება ეს ემოცია:'
+    helpQuestion.innerHTML = 'როგორ დაგეხმარება ეს ემოცია:'
+    nextButton.disabled = true
+
+    chosenEmotion = null
+}
+
+// Send a POST request by clicking on Next button, submit data, and move on to the next sentence
+
+successField = document.getElementById('ticket-submit-success')
+
+nextButton.addEventListener('click', function() {
+        if (chosenEmotion != null) {
+            let text = currentTextData.id
+            let emotion = chosenEmotion
+            let user = currentTextData.user
+            let secret = currentTextData.secret
+
+            fetch('/api/ticketrequest', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    text: text,
+                    emotion: emotion,
+                    user: user,
+                    secret: secret
+                })
+            }).then(response => response.json())
+                .then(data => {
+                    successField.style.color = '#054718'
+                    successField.innerHTML = 'მონამეცები მიღებულია (ემოცია: ' + emotionNamesData[chosenEmotion].emotion + ')'
+                    successField.style.display = 'block'
+
+                    targetCardText.style.position = 'absolute'
+                    targetCardText.animate([
+                      { transform: 'translate3D(0, 0, 0)' },
+                      { transform: 'translate3D(2000px, 0, 0)' }
+                    ], {
+                      duration: 1000,
+                      iterations: 1
+                    })
+                    setTimeout(displayNextText(), 1000)
+                    resetSelectedEmotion()
+                })
+                .catch((error) => {
+                    console.log(error)
+                    successField.style.color = '#47050b'
+                    successField.innerHTML = 'დაფიქსირდა შეცდომა'
+                    successField.style.display = 'block'
+                });
+        }
+    }
+)
